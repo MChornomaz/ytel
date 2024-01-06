14aa295efe406e9e36b96eff9d07efd900174a64
mchornomaz <chornomaz.maksym@gmail.com>
2024-01-06 19:20:52 +0200

    + sidebar  layout

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,27 @@ flsFunctions.isWebp();
 
 
 
+const menuBtn = document.querySelector('#menuBtn');
+const menuDropdown = document.querySelector('#menuDropdown');
+const menuItems = document.querySelectorAll('.dropdown__item')
+
+let menuIsShown = false;
+
+menuBtn.addEventListener('click', (event) => {
+    menuDropdown.classList.toggle('active');
+    menuIsShown = menuDropdown.classList.contains('active');
+    event.stopPropagation();
+});
+
+menuItems.forEach(el => el.addEventListener('click', (event) => {
+    menuDropdown.classList.toggle('active');
+    menuIsShown = menuDropdown.classList.contains('active');
+    event.stopPropagation();
+}));
+
+document.addEventListener('click', () => {
+    if (menuIsShown) {
+        menuDropdown.classList.remove('active');
+        menuIsShown = true;
+    }
+});
